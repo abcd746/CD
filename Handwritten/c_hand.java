d5d382f55e86c3c303a795126ecc771805959fda
Neel Jayesh Khatri <134832151+o-Erebus@users.noreply.github.com>
2025-05-01 20:15:01 +0530

    Add files via upload

---
+++ Handwritten/c_hand.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.regex.*;
+import java.nio.file.*;
+public class c_hand {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            try {
+                String code = Files.readString(Path.of(args[0]));
+                String regex = "(?<KEY>\\bint\\b|\\bfloat\\b|\\bif\\b)" + "|(?<ID>[a-zA-z_][a-zA-z0-9]*)" + "|(?<NUM>[0-9]+)" + "|(?<OP>[=+\\-*/])" + "|(?<DIL>[(){};])";
+
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(code);
+                while(matcher.find()){
+                    if(matcher.group("KEY") != null) System.out.println("KEY\t"+matcher.group());
+                    else if(matcher.group("ID") != null) System.out.println("ID\t"+matcher.group());
+                    else if(matcher.group("NUM") != null) System.out.println("NUM\t"+matcher.group());
+                    else if(matcher.group("OP") != null) System.out.println("OP\t"+matcher.group());
+                    else if(matcher.group("DIL") != null) System.out.println("DIL\t"+matcher.group());
+                    
+                }
+            } catch (Exception e) {
+                System.err.println("Error reading file: " + e.getMessage());
+            }
+        }
+    }
+}
